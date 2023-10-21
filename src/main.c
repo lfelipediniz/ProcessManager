56833c09f1194b5126bc848bbeb7af315a7ad097
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2023-10-21 02:09:21 -0300

    main finished

--- src/main.c
+++ src/main.c
@@ -2,21 +2,131 @@
 
 int main() {
    char operation[15];
+   char option[2];
+   bool flag;
 
    // get the operation from the user and store it in the variable operation
-   // while operation != quit
    while (strcmp(operation, "quit") != 0) {
       scanf("%s", operation);
 
-      // If the operation is add
+      // if the operation is add
       if (strcmp(operation, "add") == 0) {
+         int priority, hour, minute, second;
+         char description[50];
 
-         int priority;
-         TIME * time = create_time();
-         
+         // get the priority, hour, minute, second, and description from the
+         scanf("%d %d:%d:%d %s", &priority, &hour, &minute, &second,
+               description);
+
+         // storage time information in the time struct
+         TIME* time = create_time(hour, minute, second);
+
+         flag = add_process(priority, time, description);
+
+         // verify if the process was added
+         if (!flag) {
+            printf("Process not added :(\n");
+         }
       }
 
+      // if the operation is exec
+      else if (strcmp(operation, "exec") == 0) {
+         // capturing the option flag
+         scanf("%s", option);
 
+         // if the option is -p, call the execute highest priority function
+         if (strcmp(option, "-p") == 0) {
+            executeHighPriority_process();
+         }
+
+         // if the option is -t, call the execute execute low time function
+         else if (strcmp(option, "-t") == 0) {
+            executeLowTime_process();
+         }
+
+         // otherwise, invalid option
+         else {
+            printf("Invalid option\n");
+         }
+      }
+
+      // if operation is next
+      else if (strcmp(operation, "next") == 0) {
+         // capturing the option flag
+         scanf("%s", option);
+
+         // if the option is -p, call the next highest priority function
+         if (strcmp(option, "-p") == 0) {
+            infoHighPriority_process();
+         }
+         // if the option is -t, call the next low time function
+         else if (strcmp(option, "-t") == 0) {
+            infoLowTime_process();
+         }
+
+         // otherwise, invalid option
+         else {
+            printf("Invalid option\n");
+         }
+      }
+
+      // if operation is change
+      else if (strcmp(operation, "change") == 0) {
+         // capturing the option flag
+         scanf("%s", option);
+
+         // if the option is -p
+         if (strcmp(option, "-p") == 0) {
+            int oldPriority, newPriority;
+            scanf("%d|%d", &oldPriority, &newPriority);
+
+            flag = changePriority_process(oldPriority, newPriority);
+
+            // verify if the priority was changed
+            if (!flag) {
+               printf("Priority not changed :(\n");
+            }
+         }
+
+         // if the option is -t
+         else if (strcmp(option, "-t") == 0) {
+            int oldHour, oldMinute, oldSecond, newHour, newMinute, newSecond;
+            scanf("%d:%d:%d|%d:%d:%d", &oldHour, &oldMinute, &oldSecond,
+                  &newHour, &newMinute, &newSecond);
+
+            // storage time information in the time struct
+            TIME* oldTime = create_time(oldHour, oldMinute, oldSecond);
+            TIME* newTime = create_time(newHour, newMinute, newSecond);
+
+            flag = changeTime_process(oldTime, newTime);
+
+            // verify if the time was changed
+            if (!flag) {
+               printf("Time not changed :(\n");
+            }
+         }
+      }
+
+      // if operation is print
+      else if (strcmp(operation, "print") == 0) {
+         // capturing the option flag
+         scanf("%s", option);
+
+         // if the option is -p
+         if (strcmp(option, "-p") == 0) {
+            printDescPriority_process(); 
+         }
+
+         // if the option is -t
+         else if (strcmp(option, "-t") == 0) {
+            printAscTime_process();
+         }
+
+         // otherwise, invalid option
+         else {
+            printf("Invalid option\n");
+         }
+      }
    }
 
    return 0;
--- src/processManager.c
+++ src/processManager.c
@@ -2,4 +2,10 @@
 #include "../inc/processManager.h"
 
 // create a time function that returns a pointer to a TIME struct
-TIME* createTime(int hour, int minute, int second);
+TIME* createTime(int hour, int minute, int second){
+    TIME * time = (TIME *) malloc(sizeof(TIME));
+    time->hour = hour;
+    time->minute = minute;
+    time->second = second;
+    return time;
+}
