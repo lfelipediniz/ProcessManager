71d57862c11d5f5c2fdc034c854fe4fe58a9add4
Arthur Trottmann Ramos <arthurtrottmann17@gmail.com>
2023-11-01 13:14:51 -0300

    Update processManager.c

--- src/processManager.c
+++ src/processManager.c
@@ -9,39 +9,19 @@ typedef struct sheet_ {
 } SHEET;
 
 // storage processes sheets
-typedef struct process_ {
+struct process_ {
    SHEET* processesOrgPrior[MAX_PROCESSES];
    SHEET* processesOrgTime[MAX_PROCESSES];
    int size;
-} PROCESS;
-
-// create a time function that returns a pointer to a TIME struct
-TIME* createTime(int hour, int minute, int second) {
-   TIME* time = (TIME*)malloc(sizeof(TIME));
-   time->hh = hour;
-   time->mm = minute;
-   time->ss = second;
-   return time;
-}
+};
 
 // create a process function that returns a pointer to a PROCESS struct
 PROCESS* createList_process() {
    PROCESS* list = (PROCESS*)malloc(sizeof(PROCESS));
-   list->size = 0;
-   return list;
-}
 
-// compare two times
-int compareTime(const TIME* time1, const TIME* time2) {
-   // -1 time1 < time2
-   // 1 time1 > time2
-   if (time1->hh < time2->hh) return -1;
-   if (time1->hh > time2->hh) return 1;
-   if (time1->mm < time2->mm) return -1;
-   if (time1->mm > time2->mm) return 1;
-   if (time1->ss < time2->ss) return -1;
-   if (time1->ss > time2->ss) return 1;
-   return 0;  // if time1 == time2
+   if(list != NULL)
+      list->size = 0;
+   return list;
 }
 
 void shellSort(SHEET* arr[], int n, char typeCompare) {
@@ -80,6 +60,9 @@ bool add_process(PROCESS* list, int priority, TIME* time, char* description) {
    if (list == NULL || list->size == MAX_PROCESSES) return false;
 
    SHEET* sheet = (SHEET*)malloc(sizeof(SHEET));
+
+   if(sheet == NULL)
+      return false;
    sheet->prior = priority;
    sheet->start = time;
    strcpy(sheet->description, description);
@@ -87,6 +70,7 @@ bool add_process(PROCESS* list, int priority, TIME* time, char* description) {
    list->processesOrgPrior[list->size] = sheet;
    list->processesOrgTime[list->size] = sheet;
 
+   // sort each list
    shellSort(list->processesOrgPrior, list->size + 1, 'p');
    shellSort(list->processesOrgTime, list->size + 1, 't');
 
@@ -97,25 +81,31 @@ bool add_process(PROCESS* list, int priority, TIME* time, char* description) {
 
 // execute a process with highest priority
 void executeHighPriority_process(PROCESS* list) {
-   if (list == NULL || list->size == 0) {
-      printf("No processes to execute.\n");
+   if(list == NULL || list->size == 0) {
+      printf("No processes to execute\n");
       return;
    }
 
-   int priot = ((list->processesOrgPrior)[list->size - 1])->prior;
-   (list->processesOrgPrior)[list->size - 1] = NULL;
+   SHEET* aux = list->processesOrgPrior[list->size-1];
+
+   int prior = list->processesOrgPrior[list->size-1]->prior;
+   list->processesOrgPrior[list->size-1] = NULL;
 
    int i = 0;
-   for(; i < list->size; ++i) {
-      if(priot == ((list->processesOrgTime)[i])->prior) {
-         (list->processesOrgTime)[i] = NULL;
+
+   for(; i<list->size; i++) {
+      if(prior == list->processesOrgTime[i]->prior) {
+         list->processesOrgTime[i] = NULL;
          break;
       }
    }
 
-   for(; i < list->size - 1; ++i) {
-      (list->processesOrgTime)[i] = (list->processesOrgTime)[i+1];
+   for(; i<list->size-1; i++) {
+      list->processesOrgTime[i] = list->processesOrgTime[i+1];
    }
+
+   freeTime(&(aux->start));
+   free(aux);
 
    list->size--;
 }
@@ -123,14 +113,18 @@ void executeHighPriority_process(PROCESS* list) {
 // execute a process with lowest time
 void executeLowTime_process(PROCESS* list) {
    if (list == NULL || list->size == 0) {
-      printf("No processes to execute.\n");
+      printf("No processes to execute\n");
       return;
    }
 
+   SHEET* aux = list->processesOrgTime[0];
+
+   // Pointer of the item with the low time points to NULL
    TIME* t = ((list->processesOrgTime)[0])->start;
    (list->processesOrgTime)[0] = NULL;
 
    int i = 0;
+   // search for the time in the prior list
    for(; i < list->size; ++i) {
       if(compareTime(t, ((list->processesOrgPrior)[i])->start) == 0) {
          (list->processesOrgPrior)[i] = NULL;
@@ -138,78 +132,63 @@ void executeLowTime_process(PROCESS* list) {
       }
    }
 
+   // shift the lists to the left
    for(; i < list->size - 1; ++i) {
       (list->processesOrgPrior)[i] = (list->processesOrgPrior)[i+1];
    }
 
-   for(int i = 0; i < list->size - 1; ++i) {
+   for(i = 0; i < list->size - 1; ++i) {
       (list->processesOrgTime)[i] = (list->processesOrgTime)[i+1];
    }
+
+   freeTime(&(aux->start));
+   free(aux);
 
    list->size--;
 }
 
 // shows all information about the highest priority process
 void infoHighPriority_process(PROCESS* list) {
-   if (list == NULL || list->size == 0) {
-      printf("No processes to execute.\n");
-      return;
-   }
-
-   int priot = ((list->processesOrgPrior)[list->size - 1])->prior;
-
-   int i = 0;
-   for(; i < list->size; ++i) {
-      if(priot == ((list->processesOrgTime)[i])->prior) {
-         printf("%d %02d:%02d:%02d %s\n",
-               ((list->processesOrgTime)[i])->prior, ((list->processesOrgTime)[i])->start->hh, ((list->processesOrgTime)[i])->start->mm, ((list->processesOrgTime)[i])->start->ss,
-               ((list->processesOrgTime)[i])->description);
-         break;
-      }
+   if(list != NULL) {
+      printf("%02d ", list->processesOrgPrior[list->size-1]->prior);
+      printTime(list->processesOrgPrior[list->size-1]->start);
+      printf(" %s\n", list->processesOrgPrior[list->size-1]->description);
    }
 }
 
 // shows all information about the lowest time process
 void infoLowTime_process(PROCESS* list) {
-      if (list == NULL || list->size == 0) {
-         printf("No processes to execute.\n");
-         return;
-      }
-
-      TIME* t = ((list->processesOrgTime)[0])->start;
-
-      int i = 0;
-      for(; i < list->size; ++i) {
-         if(compareTime(t, ((list->processesOrgPrior)[i])->start) == 0) {
-            printf("%d %02d:%02d:%02d %s\n",
-                  ((list->processesOrgTime)[i])->prior, ((list->processesOrgTime)[i])->start->hh, ((list->processesOrgTime)[i])->start->mm, ((list->processesOrgTime)[i])->start->ss,
-                  ((list->processesOrgTime)[i])->description);
-            break;
-         }
-      }
+   if(list != NULL) {
+      printf("%02d ", list->processesOrgTime[0]->prior);
+      printTime(list->processesOrgTime[0]->start);
+      printf(" %s\n", list->processesOrgTime[0]->description);
    }
 }
 
 // change the priority of a process
 bool changePriority_process(PROCESS* list, int oldPriority, int newPriority) {
-   int start, middle, final;
+   if(list != NULL) {
+      int start, middle, final;
 
-   start = 0;
-   final = list->size - 1;
-   middle = (start + final)/2;
-
-   while((start < final) && (list->processesOrgPrior[middle]->prior != oldPriority)) {
-      if(list->processesOrgPrior[middle]->prior < oldPriority)
-         start = middle + 1;
-      else
-         final = middle - 1;
+      start = 0;
+      final = list->size - 1;
       middle = (start + final)/2;
-   }
 
-   if((start <= final) && (list->processesOrgPrior[middle]->prior == oldPriority)) {
-      list->processesOrgPrior[middle]->prior = newPriority;
-      shellSort(list->processesOrgPrior, list->size, 'p');
-      return true;
+      // Binary search to find the sheet with the old prior
+      while((start < final) && (list->processesOrgPrior[middle]->prior != oldPriority)) {
+         if(list->processesOrgPrior[middle]->prior < oldPriority)
+            start = middle + 1;
+         else
+            final = middle - 1;
+         middle = (start + final)/2;
+      }
+
+      // if it was founded, change the prior and sort the list
+      if((start <= final) && (list->processesOrgPrior[middle]->prior == oldPriority)) {
+         list->processesOrgPrior[middle]->prior = newPriority;
+         shellSort(list->processesOrgPrior, list->size, 'p');
+         return true;
+      }
    }
 
    return false;
@@ -217,47 +196,27 @@ bool changePriority_process(PROCESS* list, int oldPriority, int newPriority) {
 
 // change the time of a process
 bool changeTime_process(PROCESS* list, TIME* oldTime, TIME* newTime) {
-   int start, middle, final;
+   if(list != NULL) {
+      int start, middle, final;
 
-   start = 0;
-   final = list->size-1;
-   middle = (start + final)/2;
-
-   while((start < final) && (list->processesOrgTime[middle]->start->hh != oldTime->hh)) {
-      if(list->processesOrgTime[middle]->start->hh < oldTime->hh)
-         start = middle + 1;
-      else
-         final = middle-1;
+      start = 0;
+      final = list->size-1;
       middle = (start + final)/2;
-   }
 
-   if((start <= final) && (list->processesOrgTime[middle]->start->hh == oldTime->hh)) {
-      while((start < final) && (list->processesOrgTime[middle]->start->mm != oldTime->mm)) {
-         if(list->processesOrgTime[middle]->start->mm < oldTime->mm)
+      // binary search with the function compareTime
+      while((start < final) && (compareTime(list->processesOrgTime[middle]->start, oldTime) != 0)) {
+         if(compareTime(list->processesOrgTime[middle]->start, oldTime) == -1)
             start = middle + 1;
          else
-            final = middle-1;
+            final = middle - 1;
          middle = (start + final)/2;
       }
-         
-      if((start <= final) && (list->processesOrgTime[middle]->start->mm == oldTime->mm)) {
-         while((start < final) && (list->processesOrgTime[middle]->start->ss != oldTime->ss)) {
-            if(list->processesOrgTime[middle]->start->ss < oldTime->ss)
-               start = middle + 1;
-            else
-               final = middle-1;
-            middle = (start + final)/2;
-         }
 
-         if((start <= final) && (list->processesOrgTime[middle]->start->ss == oldTime->ss)) {
-            list->processesOrgTime[middle]->start->hh = newTime->hh;
-            list->processesOrgTime[middle]->start->mm = newTime->mm;
-            list->processesOrgTime[middle]->start->ss = newTime->ss;
-
+      // change the info and sort the list
+      if((start <= final) && (compareTime(list->processesOrgTime[middle]->start, oldTime) == 0)) {
+         if(changeInfo(list->processesOrgTime[middle]->start, newTime));
             shellSort(list->processesOrgTime, list->size, 't');
-
-            return true;
-         }
+         return true;
       }
    }
 
@@ -267,46 +226,45 @@ bool changeTime_process(PROCESS* list, TIME* oldTime, TIME* newTime) {
 // print all processes in descending order of priority
 void printDescPriority_process(PROCESS* list) {
    if (list == NULL || list->size == 0) {
-      printf("No processes to print.\n");
+      printf("No processes to print\n");
       return;
    }
 
    int i;
    for (int i = list->size - 1; i >= 0; i--) {
       SHEET* sheet = list->processesOrgPrior[i];
-      printf("%d %02d:%02d:%02d %s\n",
-             sheet->prior, sheet->start->hh, sheet->start->mm, sheet->start->ss,
-             sheet->description);
+      printf("%02d ", sheet->prior);
+      printTime(sheet->start);
+      printf(" %s\n", sheet->description);
    }
 }
 
 // print all processes in ascending order of time
 void printAscTime_process(PROCESS* list) {
    if (list == NULL || list->size == 0) {
-      printf("No processes to print.\n");
+      printf("No processes to print\n");
       return;
    }
 
    int i;
    for (i = 0; i < list->size; i++) {
       SHEET* sheet = list->processesOrgTime[i];
-      printf("%d %02d:%02d:%02d %s\n",
-             sheet->prior, sheet->start->hh, sheet->start->mm, sheet->start->ss,
-             sheet->description);
+      printf("%02d ", sheet->prior);
+      printTime(sheet->start);
+      printf(" %s\n", sheet->description);
    }
 }
 
-// free the process list
+//free the process list
 bool freeProcessList(PROCESS** list) {
    if (*list == NULL) 
       return false;
 
    for (int i = 0; i < (*list)->size; i++) {
-      free((*list)->processesOrgPrior[i]->start);
+      freeTime(&((*list)->processesOrgPrior[i]->start));
       free((*list)->processesOrgPrior[i]);
    }
 
    free(*list);
    return true;
 }
-
